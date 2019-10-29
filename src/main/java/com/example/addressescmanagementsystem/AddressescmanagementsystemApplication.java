f6c92f8b903eaba14c78516160d694395a036843
jameslin12357 <jameslin12357@qq.com>
2019-10-29 16:18:25 +0800

    cm1

---
+++ src/main/java/com/example/addressescmanagementsystem/AddressescmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.addressescmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressescmanagementsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressescmanagementsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/addressescmanagementsystem/AddressescmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addressescmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressescmanagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
